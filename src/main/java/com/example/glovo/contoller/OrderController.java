3fba624634d67812ada6f2125502360fa2f39986
JzomitiZumatti <evgeniyshinkar@gmail.com>
2024-01-27 23:58:32 +0200

    - Implement Order and Product services with respective models and controllers
    This commit introduces the 'OrderService' and 'ProductService' classes alongside with their corresponding models 'Order' and 'Product'. The services handle basic CRUD operations for their respective entities. Each service is associated with a controller which exposes the service functionality through RESTful APIs.

---
+++ src/main/java/com/example/glovo/contoller/OrderController.java
@@ -0,0 +1,54 @@
+package com.example.glovo.contoller;
+
+import com.example.glovo.model.Order;
+import com.example.glovo.model.Product;
+import com.example.glovo.service.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private OrderService orderService;
+
+    @GetMapping("/{id}")
+    public Order get(@PathVariable long id) {
+        return orderService.get(id);
+    }
+    @GetMapping
+    public Map<Long, Order> get() {
+        return orderService.get();
+    }
+
+    @PostMapping
+    public void save(Order order) {
+        orderService.save(order);
+    }
+
+    @PutMapping
+    public Order update(@RequestBody Order order) {
+        return orderService.update(order);
+    }
+
+    @PatchMapping("/{orderId}/products/{productId}")
+    public Order add(@PathVariable long orderId, @PathVariable long productId) {
+        return orderService.add(orderId, productId);
+    }
+
+    @PatchMapping("/{id}/products")
+    public Order add(@PathVariable long id, @RequestBody Product product) {
+        return orderService.add(id, product);
+    }
+
+    @DeleteMapping("/{orderId}/products/{productId}")
+    public Order delete(@PathVariable long orderId, @PathVariable long productId) {
+        return orderService.delete(orderId, productId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        orderService.delete(id);
+    }
+}
--- src/main/java/com/example/glovo/model/Product.java
+++ src/main/java/com/example/glovo/model/Product.java
@@ -1,4 +1,15 @@
 package com.example.glovo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Builder;
+
+@AllArgsConstructor
+@Data
+@Builder
 public class Product {
+    private long id;
+    private String category;
+    private String name;
+    private double price;
 }
